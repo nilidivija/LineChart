c2b0d55113ab38ffa19f35edf152b050bc345fb9
rs <rs@INDIA-MSII58BG3>
2023-02-07 11:30:58 +0530

    Add project files.

---
+++ Data/SummaryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LineChart.Data
+{
+    public class SummaryData
+    {
+        public string Team { get; set; }
+        public string MeetingLeadSource { get; set; }
+        public string MeetingMonth { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Data/SummaryDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace LineChart.Data
+{
+    public class SummaryDataService
+    {
+        public IEnumerable<SummaryData> summaryDatas { get; set; }
+        public Task<IEnumerable<SummaryData>> GetSummaryDataAsync()
+        {
+            string FilePath = "";
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(FilePath);
+            string text = xmldoc.DocumentElement.InnerText;
+            summaryDatas = JsonConvert.DeserializeObject<IEnumerable<SummaryData>>(text);
+            return Task.FromResult(summaryDatas);
+        }
+    }
+}
